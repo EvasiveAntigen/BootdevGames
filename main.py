7d1b65ac0468f63b6536f07932d0d77faf6db2de
EvasiveAntigen <galetesting@outlook.com>
2026-09-09 11:20:48 +0930

    additional of asteroids - no collisions

--- main.py
+++ main.py
@@ -4,18 +4,26 @@
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import pygame
+
+
 def main():
 	pygame.init()
 	print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
 	print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	clock = pygame.time.Clock()
-	dt : int = 0.0
+	dt : float = 0.0
+	asteroids = pygame.sprite.Group()
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
+	Asteroid.containers = (asteroids, updatable, drawable)
+	AsteroidField.containers = updatable
 	Player.containers = (updatable, drawable)
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT /2)
+	asteroid_field = AsteroidField()
 	while True:
 		log_state()
 
