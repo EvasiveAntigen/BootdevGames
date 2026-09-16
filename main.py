c8ebc198e02627dc00ca64c9e625c2ddf871afeb
EvasiveAntigen <galetesting@outlook.com>
2026-09-16 10:29:38 +0930

    added destroying asteroids

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
 				log_event("player_hit")
 				print("Game over!")
 				sys.exit()
+			for shot in shots:
+				if shot.collides_with(asteroid):
+					log_event("asteroid_shot")
+					asteroid.kill()
+					shot.kill()
 		updatable.update(dt)
 		screen.fill("black")
 		for i in drawable:
